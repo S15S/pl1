4895441b90bea94460439ed9354dd1c6349854f9
S15S <91283910+S15S@users.noreply.github.com>
2022-02-02 19:55:03 -0500

    Update StoryMapJS.js

--- story/StoryMapJS.js
+++ story/StoryMapJS.js
@@ -1,8 +1,16 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
+
+  // The location of Hampton Court Palace
+  const Hampton = { lat: 51.4036, lng: 0.3378 };
+  // The map, centered
+   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: { lat: 51.4036, lng: 0.3378 },
   });
-  
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: Hampton,
+    map: map,
+  });
 }
 
