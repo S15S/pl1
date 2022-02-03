9c86251b35b929365a3830f1737f07dcc9fa0095
S15S <91283910+S15S@users.noreply.github.com>
2022-02-02 21:37:26 -0500

    Update StoryMapJS.js

--- story/StoryMapJS.js
+++ story/StoryMapJS.js
@@ -1,18 +1,11 @@
 function initMap() {
-  // The map, centered
+  // The map, centered on London
   // Reference for attempted marker: https://developers.google.com/maps/documentation/javascript/examples/marker-simple
    const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
     center: { lat: 51.5072, lng: 0.1276 },
   });
   
-  // The marker, positioned at London
-  new google.maps.Marker({
-    position: London,
-    map,
-    title: "London!",
-  });
-}
 const Museumcoords = [
     { lat: 51.4967, lng: 0.1764 },
     { lat: 51.4966, lng: 0.1722 },
