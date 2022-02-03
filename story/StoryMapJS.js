21053d71ea635515d2b4bace7498491d050b2122
S15S <91283910+S15S@users.noreply.github.com>
2022-02-02 20:53:37 -0500

    Update StoryMapJS.js

--- story/StoryMapJS.js
+++ story/StoryMapJS.js
@@ -1,12 +1,15 @@
 function initMap() {
 
-  // The location of Hampton Court Palace
+  // The location of London.  This is not centered properly, I'm not sure why nor how to fix it.
   const London = { lat: 51.5072, lng: 0.1276 };
+  
   // The map, centered
+  // Reference for attempted marker: https://developers.google.com/maps/documentation/javascript/examples/marker-simple
    const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 11,
+    zoom: 13,
     center: London,
   });
+  
   // The marker, positioned at London
   new google.maps.Marker({
     position: London,
