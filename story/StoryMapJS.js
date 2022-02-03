6b2f15ad19834543a149e129c50514988d572cc9
S15S <91283910+S15S@users.noreply.github.com>
2022-02-02 21:31:59 -0500

    Update StoryMapJS.js

--- story/StoryMapJS.js
+++ story/StoryMapJS.js
@@ -1,13 +1,9 @@
 function initMap() {
-
-  // The location of London.  This is not centered properly, I'm not sure why nor how to fix it.
-  const London = { lat: 51.5072, lng: 0.1276 };
-  
   // The map, centered
   // Reference for attempted marker: https://developers.google.com/maps/documentation/javascript/examples/marker-simple
    const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
-    center: London,
+    center: { lat: 51.5072, lng: 0.1276 },
   });
   
   // The marker, positioned at London
