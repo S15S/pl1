3054ac4bc96e6d86e369c5760d60f43fa2169f65
S15S <91283910+S15S@users.noreply.github.com>
2022-02-02 21:16:07 -0500

    Update StoryMapJS.js

--- story/StoryMapJS.js
+++ story/StoryMapJS.js
@@ -6,7 +6,7 @@ function initMap() {
   // The map, centered
   // Reference for attempted marker: https://developers.google.com/maps/documentation/javascript/examples/marker-simple
    const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
+    zoom: 8,
     center: London,
   });
   
@@ -16,5 +16,22 @@ function initMap() {
     map,
     title: "London!",
   });
+}
+const Museumcoords = [
+    { lat: 51.4967, lng: 0.1764 },
+    { lat: 51.4966, lng: 0.1722 },
+    { lat: 51.5194, lng: 0.1270 },
+    { lat: 51.4967, lng: 0.1764 },
+  ];
+  const TriMuseums = new google.maps.Polygon({
+    paths: Museumcoords,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+  });
+
+  TriMuseums.setMap(map);
 }
 
